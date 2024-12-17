d5fc65d709fc9eabf95c6a6316dac54671d644f9
Gomnonix <lee20481997@gmail.com>
2024-12-17 17:25:03 +0900

    관리자 기능 add

--- app/fitplans/admin.py
+++ app/fitplans/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from fitplans.models import Fitplan, Goal
+
+@admin.register(Fitplan)
+class FitplanAdmin(admin.ModelAdmin):
+    list_display = ('id', 'date', 'weight', 'target_weight', 'goal', 'period')
+
+@admin.register(Goal)
+class GoalAdmin(admin.ModelAdmin):
+    list_display = ('id', 'goal')
+    search_fields = ('goal',)
