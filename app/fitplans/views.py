6502e5b42dad4fe461811b554ab82c4ae21ddb85
Gomnonix <lee20481997@gmail.com>
2024-12-28 20:59:44 +0900

    경로 설정

--- app/fitplans/views.py
+++ app/fitplans/views.py
@@ -1,10 +1,10 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from services.openai_service import generate_fitness_plan
 
 from .models import Fitplan
 from .serializers import FitPlanSerializer
+from .services.openai_service import generate_fitness_plan
 
 
 class FitplanPostView(APIView):
