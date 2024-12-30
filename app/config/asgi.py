8db1b4504e825b754cf6151414c92272d5028b1b
Gomnonix <lee20481997@gmail.com>
2024-12-30 15:24:33 +0900

    채팅 구현(2)

--- app/config/asgi.py
+++ app/config/asgi.py
@@ -1,5 +1,5 @@
 import os
-
+import django
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
@@ -7,6 +7,7 @@ from django.core.asgi import get_asgi_application
 from chat.routing import websocket_urlpatterns
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+django.setup()
 
 application = ProtocolTypeRouter(
     {
