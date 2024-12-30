7766cf7f70475eeb7e748a4d26ad5f4093506729
Gomnonix <lee20481997@gmail.com>
2024-12-30 18:01:44 +0900

    채팅 데이터 응답

--- app/chat/views.py
+++ app/chat/views.py
@@ -4,7 +4,7 @@ from .models import Message
 
 
 def get_messages(request, room_name):
-    messages = Message.objects.filter(room=room_name).order_by("-timestamp")[:50]
+    messages = Message.objects.filter(room__name=room_name).order_by("-timestamp")[:50]
     return JsonResponse(
-        {"messages": list(messages.values("user__username", "content", "timestamp"))}
+        {"messages": list(messages.values("user__name", "content", "timestamp"))}
     )
--- app/config/urls.py
+++ app/config/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("v1/users/", include("users.urls")),
     path("v1/fitplans/", include("fitplans.urls")),
+    path("v1/chat/", include("chat.urls")),
 ]
